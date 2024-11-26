e846c669887f391a7fad2c0c9a130d94d63f5d4d
huhuwang <1159013842@qq.com>
2024-11-26 14:29:33 +0800

    add backend(security)

--- src/main/java/com/hayes/pvtsys/controller/DownloadFileController.java
+++ src/main/java/com/hayes/pvtsys/controller/DownloadFileController.java
@@ -5,11 +5,11 @@ import com.hayes.pvtsys.service.DownloadService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/download")
 public class DownloadFileController {
 
