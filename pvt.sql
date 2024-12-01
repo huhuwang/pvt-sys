8a803b78bc052af7aa4442404dd76684e94f9f9a
huhuwang <1159013842@qq.com>
2024-12-01 21:17:58 +0800

    add backend(security)

--- pvt.sql
+++ pvt.sql
@@ -11,7 +11,7 @@
  Target Server Version : 90001 (9.0.1)
  File Encoding         : 65001
 
- Date: 26/11/2024 18:58:49
+ Date: 01/12/2024 21:14:11
 */
 
 SET NAMES utf8mb4;
@@ -26,11 +26,13 @@ CREATE TABLE `common_test_case_base`  (
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '描述',
   `summary` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '简述',
   `expected_result` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '期待结果',
-  `category` int NULL DEFAULT NULL COMMENT '1. SIT 2.UAT 4.RT 8.PVT 16.WEB 32.IPAD',
+  `category` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '1. SIT 2.UAT 4.RT 8.PVT 16.WEB 32.IPAD',
+  `category_val` int NULL DEFAULT NULL COMMENT '1. SIT 2.UAT 4.RT 8.PVT 16.WEB 32.IPAD',
   `priority` tinyint NULL DEFAULT NULL COMMENT '1. Urgent 2.Highest 3 High 4.low 5Lowest 6.Blocker 7.Critical 8Major 9.Minor 10.Trivial',
   `type` tinyint NULL DEFAULT NULL COMMENT '1. related to story 2. common  ',
   `row_height` int NULL DEFAULT NULL COMMENT '行高',
   `rt_flow` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '如果是RT case,对应的RT flow',
+  `step` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '步骤',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `create_user` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建者',
   `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
@@ -38,7 +40,7 @@ CREATE TABLE `common_test_case_base`  (
   `status` tinyint NULL DEFAULT 1 COMMENT '状态  1.启用   2.禁用  3.完成',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 41 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for deployment
@@ -59,30 +61,7 @@ CREATE TABLE `deployment`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `create_idx`(`create_time` ASC) USING BTREE,
   INDEX `template_idx`(`template_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for document
--- ----------------------------
-DROP TABLE IF EXISTS `document`;
-CREATE TABLE `document`  (
-  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '主键',
-  `document_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '文件名称',
-  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '描述',
-  `result_id` int NULL DEFAULT NULL COMMENT 'result id',
-  `original_size` bigint NULL DEFAULT NULL COMMENT '原始尺寸',
-  `scale_size` bigint NULL DEFAULT NULL COMMENT '处理之后的尺寸',
-  `document_type` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '文件类型',
-  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '图片地址',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `create_user` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建者',
-  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
-  `update_user` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '修改者',
-  `status` tinyint NULL DEFAULT 1 COMMENT '状态  1.启用   2.禁用  3.完成',
-  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `result_idx`(`result_id` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for kv_constants
@@ -104,23 +83,30 @@ CREATE TABLE `kv_constants`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `parent_idx`(`parent_id` ASC) USING BTREE,
   INDEX `template_idx`(`template_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 958 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for pvt_permission
+-- Table structure for pvt_document
 -- ----------------------------
-DROP TABLE IF EXISTS `pvt_permission`;
-CREATE TABLE `pvt_permission`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '注释',
-  `permission_name` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '权限名称',
-  `permission_code` varchar(46) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '权限编号',
-  `permission_url` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '功能url',
-  `permission_describe` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '权限描述',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '新建时间',
+DROP TABLE IF EXISTS `pvt_document`;
+CREATE TABLE `pvt_document`  (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '主键',
+  `document_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '文件名称',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '描述',
+  `result_id` int NULL DEFAULT NULL COMMENT 'result id',
+  `original_size` bigint NULL DEFAULT NULL COMMENT '原始尺寸',
+  `scale_size` bigint NULL DEFAULT NULL COMMENT '处理之后的尺寸',
+  `document_type` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '文件类型',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '图片地址',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `create_user` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建者',
   `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `update_user` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '修改者',
+  `status` tinyint NULL DEFAULT 1 COMMENT '状态  1.启用   2.禁用  3.完成',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `IDX_CODE`(`permission_code` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+  INDEX `result_idx`(`result_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for pvt_role
@@ -133,7 +119,7 @@ CREATE TABLE `pvt_role`  (
   `create_time` datetime NULL DEFAULT NULL COMMENT '新建时间',
   `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for pvt_role_permission
@@ -148,7 +134,7 @@ CREATE TABLE `pvt_role_permission`  (
   `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `IDX_PRINCIPAL`(`principal_id` ASC, `permission_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1054 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for pvt_user
@@ -167,7 +153,7 @@ CREATE TABLE `pvt_user`  (
   `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `user_account_idx`(`account` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for rt_summary_template
@@ -185,7 +171,7 @@ CREATE TABLE `rt_summary_template`  (
   `status` tinyint NULL DEFAULT NULL COMMENT '状态  1.启用   2.禁用  3.完成',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 44 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for test_case
@@ -199,6 +185,7 @@ CREATE TABLE `test_case`  (
   `expected_result` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '期待结果',
   `priority` tinyint NULL DEFAULT NULL COMMENT '1. Urgent 2.Highest 3 High 4.low 5Lowest 6.Blocker 7.Critical 8Major 9.Minor 10.Trivial',
   `type` tinyint NULL DEFAULT NULL COMMENT '1. related to story 2. common  ',
+  `step` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '步骤',
   `row_height` int NULL DEFAULT NULL COMMENT '行高',
   `base_case_from` int NULL DEFAULT NULL COMMENT '来源',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
@@ -210,7 +197,7 @@ CREATE TABLE `test_case`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `ticket_no_idx`(`ticket_no` ASC) USING BTREE,
   INDEX `case_from_idx`(`base_case_from` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 199 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for test_result
@@ -220,7 +207,6 @@ CREATE TABLE `test_result`  (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
   `case_id` int NOT NULL COMMENT 'case id',
   `category` int NOT NULL COMMENT '1. SIT 2.UAT 4.RT 8.PVT 16.WEB 32.IPAD',
-  `step` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '步骤',
   `test_data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '测试数据',
   `actual_result` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '实际结果',
   `result` tinyint NULL DEFAULT NULL COMMENT '1. pass 2. no pass',
@@ -233,7 +219,7 @@ CREATE TABLE `test_result`  (
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `case_id_idx`(`case_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 903 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ticket
@@ -255,6 +241,6 @@ CREATE TABLE `ticket`  (
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `deployment_ticket_idx`(`deployment_id` ASC, `ticket_no` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 45 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
